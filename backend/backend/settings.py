4d1aa2c12b85d3570e2d1161a1009ef1206476e1
VladimirDudokin <vladimir.dudokin@yandex.ru>
2024-12-09 22:00:02 +0300

    fix2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.25.228', 'taski.plus-dev.ru', 'localhost']
+ALLOWED_HOSTS = ['158.160.25.228', 'taski.plus-dev.ru', 'localhost', 'https://taski.plus-dev.ru', '127.0.0.1']
 
 
 # Application definition
